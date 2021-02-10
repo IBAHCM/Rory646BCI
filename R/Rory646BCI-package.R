aae416ce27c7a23f216a250610052bbe701141e2
Rory646 <2188096T@student.gla.ac.uk>
2021-02-10 15:26:07 +0000

    documentation

---
+++ R/Rory646BCI-package.R
@@ -0,0 +1,16 @@
+#' Rory646BCI
+#'
+#' \code{Rory646BCI} is a dummy package developed as part of the Reproducible
+#' Programming in R course..
+#'
+#' This text will appear in the Details section of the documentation.
+#'
+#' @author
+#' Rory <2188096T@student.gla.ac.uk> (maintainer) \cr
+#'
+#' @name Rory646BCI-package
+#' @aliases Rory646BCI
+#' @docType package
+#'
+#'
+NULL
